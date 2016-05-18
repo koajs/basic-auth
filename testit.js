23072cdedb898b769ef03ab1bb8e981c28e19947
Jan Nahody <jan.nahody@gmail.com>
2016-05-18 07:46:19 +0200

    Remove temporary file

--- testit.js
+++
@@ -1,6 +0,0 @@
-
-const _ = require('lodash');
-
-let testArray = [{name: 'name1', pass: 'pass2'}, {name: 'name1', pass: 'pass1'}];
-
-console.log(_.some(testArray, {name: 'name2', pass: 'pass2'}));
